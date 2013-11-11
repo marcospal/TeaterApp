2e7205faabbea340856f48e55b3fc59122f3ebb9
Tim Garbos <timgarbos@gmail.cm>
2013-11-11 15:49:37 +0100

    production abs path

--- teaterapp/production_settings.py
+++ teaterapp/production_settings.py
@@ -5,6 +5,17 @@ LIVE = True
 
 
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': '/home/tax/webapps/contabile2test/TeaterApp/sqlite.db',                      # Or path to database file if using sqlite3.
+        # The following settings are not used with sqlite3:
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '',                      # Set to empty string for default.
+    }
+}
 
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
